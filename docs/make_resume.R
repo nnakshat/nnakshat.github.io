59941a21556b693172dd7a5279dbb31d10fd6078
Nishanth <15133031+nishanths96@users.noreply.github.com>
2022-10-24 16:16:26 -0400

    deleting content

--- docs/make_resume.R
+++
@@ -1,4 +0,0 @@
-rmarkdown::render("docs/svm-resume.Rmd", 
-                  output_file="svm-resume.pdf",
-                  rmarkdown::pdf_document(template = stevetemplates::templ_resume(), 
-                                          latex_engine = "xelatex", dev="cairo_pdf"))
